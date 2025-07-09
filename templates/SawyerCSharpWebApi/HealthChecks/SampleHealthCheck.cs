a27f3dced06e23db325e730b27d1a36dc2f62b9c
Sawyer Watts <sawyer.watts@gmail.com>
2025-07-08 19:29:53 -0700

    add try/catch to healthcheck to catch op cancelled exc

--- templates/SawyerCSharpWebApi/HealthChecks/SampleHealthCheck.cs
+++ templates/SawyerCSharpWebApi/HealthChecks/SampleHealthCheck.cs
@@ -8,6 +8,17 @@ public class SampleHealthCheck : IHealthCheck
         HealthCheckContext context,
         CancellationToken cancellationToken)
     {
-        return Task.FromResult(HealthCheckResult.Healthy());
+        try
+        {
+            var stream = new MemoryStream();
+            return Task.FromResult(HealthCheckResult.Healthy());
+        }
+        // All exceptions (except a cancellation exception) will be caught by
+        // the healthcheck library, so healthchecks only need to catch this
+        // exception (and attach helpful message).
+        catch (OperationCanceledException exc)
+        {
+            return Task.FromResult(HealthCheckResult.Degraded("The health check was cancelled, likely due to timeout", exc));
+        }
     }
 }
